312a64a01441b7134509100432575c561df4bdb8
Pritam <cooldeypritam@gmail.com>
2026-02-26 23:49:30 +0530

    Pritam added UC7

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,5 +1,19 @@
+import java.util.*;
 public class PalindromeCheckerApp {
     public static void main(String args[]){
-        System.out.println("Welcome to Palindrome Checker App Management System..");
+        String input = "refer";
+
+        Deque<Character> deque = new ArrayDeque<>();
+        for (char c : input.toCharArray())
+            deque.addLast(c);
+        boolean isPalindrome = true;
+        while (deque.size() > 1) {
+            if (!deque.removeFirst().equals(deque.removeLast())) {
+                isPalindrome = false;
+                break;
+            }
+        }
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome);
     }
 }
